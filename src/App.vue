b67e4d2df7ed30cfc95245fb4e4fa3195fc9b61a
alvinlucillo <allouothers@gmail.com>
2021-06-18 17:37:02 +0800

    Change layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+      <component v-bind:is="layout"></component>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import DiscordLayout from './layout/Discord';
+import BaseLayout from './layout/Base'
+import SystemBarLayout from './layout/SystemBar'
 
-#nav {
-  padding: 30px;
-}
+export default{
+  computed: {
+    layout(){
+      return this.$store.state.layout;
+    }
+  },
+  components: {
+    'discordLayout': DiscordLayout,
+    'baseLayout': BaseLayout,
+    'systemBarLayout': SystemBarLayout,
+  }
+};
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    layout: "baseLayout",
   },
   mutations: {
+    SET_LAYOUT(state, value){
+      state.layout = value;
+    }
   },
   actions: {
+    setLayout({commit}, data){
+      commit('SET_LAYOUT', data);
+    }
   },
   modules: {
   }
